1303d1f36d5777e5f964df4505d662dbc795a1e3
HRN_Harshit <hrnharshit@gmail.com>
2024-06-22 12:57:06 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,38 @@
-import React from 'react'
-import ProfileClass from './ProfileClass';
-import Profile from './Profile';
+import React from "react";
+import ProfileClass from "./ProfileClass";
+import Profile from "./Profile";
 
 // const About = () => {
 //   return (
 //     <div>About</div>
 //   )
 // }
- 
+
 class About extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     //console.log("Parent - constructor");
   }
-  componentDidMount(){
-     // Best place to make an Api call
+  componentDidMount() {
+    // Best place to make an Api call
     //console.log("Parent - componentDidMount");
   }
-  render(){
-     //console.log("Parent - render");
-    return(
+  render() {
+    //console.log("Parent - render");
+    return (
       <>
-      <div>
-        <h1>About</h1>
-      <p>
-          This is the Namaste React Live Course Chapter 07 - Finding the Path 🚀
-      </p>
-      </div>
-      {/* <Profile name="Ashish"/> */}
-      <ProfileClass fname="Ashish " lname={"Saha"} age={23} />
+        <div>
+          <h1>About</h1>
+          <p>This is the Namaste React Live Course Chapter 07 - Finding the Path 🚀</p>
+        </div>
+        {/* <Profile name="Ganga"/> */}
+        <ProfileClass fname="Ganga " lname={"Sagar"} age={23} />
       </>
-    )
+    );
   }
 }
 
-export default About
-
+export default About;
 
 /**
  *
